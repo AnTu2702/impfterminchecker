248bbc810e14bd9ee095abccd3bd57447f58c2a3
Andreas Tully <andreas.tully@googlemail.com>
2021-05-30 11:33:50 +0200

    Update checker_lambda.py

--- checker_lambda.py
+++ checker_lambda.py
@@ -12,8 +12,8 @@ def lambda_handler(event, context):
     try:
         response = requests.get(baseurl+vaccid)
         regex = r'(.*)' + re.escape('<div class="panel-body">') + r'(.*)' + re.escape('</div> <div class="panel-footer">') + r'(.*)'
-        htmlStr = re.sub(' +',' ', response.text.replace("\r\n","").replace("\t"," "))
-        matchObj = re.match(regex, htmlStr)
+        html = re.sub(' +',' ', response.text.replace("\r\n","").replace("\t"," "))
+        matchObj = re.match(regex, html)
         message = matchObj.group(2).strip()
     except:
         boto3.client('sns').publish(TargetArn=snsArn, Message=message, Subject=vaccine)
