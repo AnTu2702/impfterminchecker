c12b632f0d2b7491361ca4e6ca26e8dd6e4b9e6d
Andreas Tully <andreas.tully@googlemail.com>
2021-05-30 11:26:56 +0200

    Update checker_lambda.py

--- checker_lambda.py
+++ checker_lambda.py
@@ -2,24 +2,22 @@ import re, requests, boto3
 
 def lambda_handler(event, context):
 
-	vaccine = event['vaccine']
-	vaccid = event['vaccid']
-	baseurl = event['baseurl']
-	snsArn = event['snsarn']
-	
-	message = 'Bitte sofort Termine für den Impfstoff ' + vaccine + ' manuell checken!'
-	response = requests.get(baseurl+vaccid)
+    vaccine = event['vaccine']
+    vaccid = event['vaccid']
+    baseurl = event['baseurl']
+    snsArn = event['snsarn']
 
-	regex = r'(.*)' + re.escape('<div class="panel-body">') + r'(.*)' + re.escape('</div> <div class="panel-footer">') + r'(.*)'
-	collectionsHtml = re.sub(' +',' ', response.text.replace("\r\n","").replace("\t"," "))
+    message = 'Bitte sofort Termine für den Impfstoff ' + vaccine + ' manuell checken!'
+    response = requests.get(baseurl+vaccid)
 	
-	try:
-		matchObj = re.match(regex, collectionsHtml)
-		message = matchObj.group(2).strip()
-		
-	except:
-		boto3.client('sns').publish(TargetArn=snsArn, Message=message, Subject=vaccine)
-		
-	finally:
-		print(f"{baseurl}{vaccid}")
-		return f"{vaccine}: {message}"
+    regex = r'(.*)' + re.escape('<div class="panel-body">') + r'(.*)' + re.escape('</div> <div class="panel-footer">') + r'(.*)'
+    collectionsHtml = re.sub(' +',' ', response.text.replace("\r\n","").replace("\t"," "))
+
+    try:
+        matchObj = re.match(regex, collectionsHtml)
+	message = matchObj.group(2).strip()
+    except:
+        boto3.client('sns').publish(TargetArn=snsArn, Message=message, Subject=vaccine)
+    finally:
+        print(f"{baseurl}{vaccid}")
+        return f"{vaccine}: {message}"
