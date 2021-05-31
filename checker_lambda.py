dc9f8487fb65e048c15ca3adf7f15adcc7cd9cf3
Andreas Tully <andreas.tully@googlemail.com>
2021-05-31 08:41:18 +0200

    Update checker_lambda.py

--- checker_lambda.py
+++ checker_lambda.py
@@ -7,12 +7,12 @@ def lambda_handler(event, context):
     baseurl = event['baseurl']
     snsArn = event['snsarn']
 
-    message = f"Bitte sofort Termine für den Impfstoff {vaccine} manuell checken!"
+    message = f"Bitte sofort Termine für den Impfstoff {vaccine} manuell checken! + f"\r\n\r\n{baseurl}{vaccid}"
     regex = r'(.*)' + re.escape('<div class="panel-body">') + r'(.*)' + re.escape('</div> <div class="panel-footer">') + r'(.*)'
 
     try:
         response = requests.get(baseurl+vaccid)
-        message = re.match(regex, re.sub(' +',' ', response.text.replace("\r\n","").replace("\t"," "))).group(2).strip() + f"\r\n\r\n{baseurl}{vaccid}"
+        message = re.match(regex, re.sub(' +',' ', response.text.replace("\r\n","").replace("\t"," "))).group(2).strip()
     except:
         boto3.client('sns').publish(TargetArn=snsArn, Message=message, Subject=vaccine)
     finally:
