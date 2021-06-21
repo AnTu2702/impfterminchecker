226b5208d951b33436e7a431794e5f47666b643c
Andreas Tully <andreas.tully@googlemail.com>
2021-06-21 08:04:39 +0200

    Handle I55 message

--- checker_lambda.py
+++ checker_lambda.py
@@ -21,9 +21,12 @@ def lambda_handler(event, context):
             r_min = extract_from(response.text, '<span id="lblMin">', '</span> Minuten')
             r_sec = extract_from(response.text, '<span id="lblSec">', '</span> Sekunden')
             message = f"Webseite verlangt Wartezeit - Minuten {r_min} Sekunden {r_sec}. Probiere es später wieder..."
+        elif 'I-55: Keine Termine' in response.text:
+            message = f"Keine Termine vorhanden derzeit...!"
         else:
             message = extract_from(response.text, '<div class="panel-body">', '</div> <div class="panel-footer">')
     except:
         boto3.client('sns').publish(TargetArn=snsArn, Message=message, Subject=vaccine)
     finally:
         return f"{vaccine}: {message}"
+    
